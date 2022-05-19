56d30682b2bfc4f56e432c8d58135694f121dd43
CarterPlus <hashcat@126.com>
2022-05-19 23:00:37 +0800

    feat(debug-100): 初始化为springboot项目

---
+++ java_service_debug_100/src/main/java/com/example/java_service_debug_100/JavaServiceDebug100Application.java
@@ -0,0 +1,13 @@
+package com.example.java_service_debug_100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaServiceDebug100Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaServiceDebug100Application.class, args);
+    }
+
+}
---
+++ java_service_debug_100/src/test/java/com/example/java_service_debug_100/JavaServiceDebug100ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_service_debug_100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServiceDebug100ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
